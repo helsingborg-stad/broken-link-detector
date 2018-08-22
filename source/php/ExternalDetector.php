b71259ac7adf0ce4cda885e88be4f555cf22c18e
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-08-22 10:51:52 +0200

    Skip ssl validation

--- source/php/ExternalDetector.php
+++ source/php/ExternalDetector.php
@@ -191,6 +191,8 @@ class ExternalDetector
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_NOBODY, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         // Get the response
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
