8bb05cef0aacf8f098adbbab0a7a796e18a1ebb2
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-08-23 09:45:57 +0200

    Set curl user agent

--- source/php/ExternalDetector.php
+++ source/php/ExternalDetector.php
@@ -190,10 +190,11 @@ class ExternalDetector
     {
         // Init curl
         $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_NOBODY, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         // Get the response
