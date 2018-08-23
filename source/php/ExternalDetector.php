2df741d1fab0db468d5ab3a8458f2aa8ae4d5422
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-08-23 10:01:56 +0200

    Set curl option followlocation to true

--- source/php/ExternalDetector.php
+++ source/php/ExternalDetector.php
@@ -195,6 +195,7 @@ class ExternalDetector
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_HTTPGET, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         // Get the response
