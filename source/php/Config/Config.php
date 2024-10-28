2e57d15bbcecf9362e64593d9d785368e89e2d82
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-10-28 11:19:03 +0100

    fix: add isEnabledEnumList.

--- source/php/Config/Config.php
+++ source/php/Config/Config.php
@@ -5,6 +5,13 @@ namespace BrokenLinkDetector\Config;
 use WpService\Contracts\ApplyFilters;
 use WpService\Contracts\GetOption;
 
+enum Feature: string
+{
+    case SCAN_BROKEN_LINKS = 'ScanBrokenLinks';
+    case LIST_BROKEN_LINKS = 'ListBrokenLinks';
+    case FIX_INTERNAL_LINKS = 'FixInternalLinks';
+    case HIGHLIGHT_BROKEN_LINKS = 'HighlightBrokenLinks';
+}
 class Config implements ConfigInterface
 {
   public function __construct(
@@ -13,6 +20,33 @@ class Config implements ConfigInterface
     private string $pluginPath,
     private string $pluginUrl
   ){}
+
+  /**
+   * Check if a feature is enabled.
+   * 
+   * @param Feature $feature
+   */
+  public function isEnabled(Feature $feature) {
+
+    $defaultEnabled = [
+      Feature::SCAN_BROKEN_LINKS => true,
+      Feature::LIST_BROKEN_LINKS => true,
+      Feature::FIX_INTERNAL_LINKS => true,
+      Feature::HIGHLIGHT_BROKEN_LINKS => true,
+    ];
+
+    $defaultEnabled = $this->wpService->applyFilters(
+      $this->createFilterKey(__FUNCTION__), 
+      $defaultEnabled
+    );
+
+    $isEnabled = $defaultEnabled[$feature] ?? false;
+
+    return $this->wpService->applyFilters(
+        $this->createFilterKey(__FUNCTION__ . "/{$feature->value}"),
+        $isEnabled
+    );
+  }
 
   /**
    * Get the key for the database version.
@@ -128,7 +162,7 @@ class Config implements ConfigInterface
     return $this->wpService->applyFilters(
       $this->createFilterKey(__FUNCTION__), 
       ['attachment', 'revision', 'acf', 'acf-field', 'acf-field-group']
-    );
+    ) ?? [];
   }
 
   /**
