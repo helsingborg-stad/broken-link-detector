16a22e8f07a85c2bb87ec615b3ab8b7e825add81
Thor Brink <thor.brink@helsingborg.se>
2024-11-06 12:39:07 +0000

    fix: add useragent to classify request

--- source/php/BrokenLinkRegistry/ClassifyLink/Classify.test.e2e.php
+++ source/php/BrokenLinkRegistry/ClassifyLink/Classify.test.e2e.php
@@ -8,13 +8,24 @@ use WpService\Implementations\NativeWpService;
 
 class ClassifyTest extends WP_UnitTestCase
 {
-  public function testRemoteGet() {
+  /**
+   * @testdox External working URL should return 200
+   * @dataProvider externalSuccessfullUrlProvider
+   */
+  public function testSuccessRemoteFul($url) {
     $wpService = new NativeWpService();
     $config = new Config($wpService, 'filterPrefix', 'pluginPath', 'pluginUrl');
-    $classified = Classify::factory('https://www.google.com/', null, $wpService, $config);
+    $classified = Classify::factory($url, null, $wpService, $config);
 
-      $this->assertEquals(true, $classified->isExternal());
-      $this->assertEquals(false, $classified->isBroken());
-      $this->assertEquals(200, $classified->getHttpCode());
+    $this->assertEquals(200, $classified->getHttpCode());
+    $this->assertEquals(true, $classified->isExternal());
+    $this->assertEquals(false, $classified->isBroken());
+  }
+
+  private function externalSuccessfullUrlProvider():array {
+    return [
+      ['https://www.scb.se'],
+      ['https://google.com'],
+    ];
   }
 }
