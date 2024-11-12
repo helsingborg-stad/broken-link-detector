9422e1de8fd234134c84cf4668524eb3d50aae6d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-11-12 10:28:32 +0100

    fix: update interface with all functions avabile in config.

--- source/php/Config/ConfigInterface.php
+++ source/php/Config/ConfigInterface.php
@@ -7,35 +7,44 @@ use WpService\WpService;
 
 interface ConfigInterface
 {
-  public function __construct(
-    WpService $wpService, 
-    AcfService $acfService,
-    string $filterPrefix,
-    string $pluginPath,
-    string $pluginUrl,
-  );
-  public function getDatabaseVersionKey(): string;
-  public function getDatabaseVersion(): string;
-  public function getTableName(): string;
-  public function getPluginUrl(): string;
-  public function getPluginPath(): string;
-  public function getPluginTemplatePath(): string;
-  public function getTextDomain(): string;
-  public function linkUpdaterBannedPostTypes(): array;
-  public function linkDetectBannedPostTypes(): array;
-  public function linkDetectAllowedPostStatuses(): array;
+    public function __construct(
+        WpService $wpService, 
+        AcfService $acfService,
+        string $filterPrefix,
+        string $pluginPath,
+        string $pluginUrl,
+    );
 
-  /* Cli */ 
-  public function getCommandNamespace(): string;
+    public function getDatabaseVersionKey(): string;
+    public function getDatabaseVersion(): string;
+    public function getTableName(): string;
+    public function getPluginUrl(): string;
+    public function getPluginPath(): string;
+    public function getPluginTemplatePath(): string;
+    public function getPluginFieldsPath(): string;
+    public function getTextDomain(): string;
+    public function linkUpdaterBannedPostTypes(): array;
+    public function linkDetectBannedPostTypes(): array;
+    public function linkDetectAllowedPostStatuses(): array;
+    
+    /* Link classification */ 
+    public function responseCodesConsideredBroken(): array;
+    public function getRecheckInterval(): int;
 
-  /* Link classification */ 
-  public function responseCodesConsideredBroken(): array;
-  public function getRecheckInterval(): int; //The minimum time between rechecking a link
+    /* DNS lookup */
+    public function checkIfDnsRespondsBeforeProbingUrl(): bool;
 
-  /* DNS lookup */
-  public function checkIfDnsRespondsBeforeProbingUrl(): bool;
+    /* Http polling */ 
+    public function getMaxRedirects(): int;
+    public function getTimeout(): int;
 
-  /* Http polling */ 
-  public function getMaxRedirects(): int;
-  public function getTimeout(): int;
+    /* Context detection */
+    public function getDomainsThatShouldNotBeChecked(): array;
+    public function isContextCheckEnabled(): bool;
+    public function getContextCheckUrl(): string;
+    public function getContextCheckTimeout(): int;
+    public function getContextCheckDomainsToDisable(): array;
+    public function getContextCheckSuccessClass(): string;
+    public function getContextCheckFailedClass(): string;
+    public function getContextCheckTooltipText(): string;
 }
