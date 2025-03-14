d6867c22a83d1f37577c71771852396d2225766e
GitHub Action <>
2025-03-14 14:42:08 +0000

    build: update package file versions to 4.2.9

--- broken-link-detector.php
+++ broken-link-detector.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Broken Link Detector
  * Description:       Detects and fixes (if possible) broken links in post_content
- * Version: 4.2.8
+ * Version: 4.2.9
  * Author:            Sebastian Thulin
  * License:           MIT
  * License URI:       https://opensource.org/licenses/MIT
